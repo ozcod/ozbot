f4b76588e8f02e488842d8b46fea0a47473659c4
Ozair Ahmad <ozair.ahmad@stud.uni-bamberg.de>
2026-04-23 23:11:52 +0200

    feature: Add pineconeDB and embeddings

--- prepare.js
+++ prepare.js
@@ -1,6 +1,39 @@
 import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
+import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
+import { Pinecone as PineconeClient } from "@pinecone-database/pinecone";
+import { PineconeStore } from "@langchain/pinecone";
+import { GoogleGenerativeAIEmbeddings } from "@langchain/google-genai";
+import { TaskType } from "@google/generative-ai";
+
+const embeddings = new GoogleGenerativeAIEmbeddings({
+  modelName: "embedding-001",
+  taskType: TaskType.RETRIEVAL_DOCUMENT,
+});
+
+const pinecone = new PineconeClient();
+const pineconeIndex = pinecone.Index(process.env.PINECONE_INDEX_NAME);
+const vectorStore = await PineconeStore.fromExistingIndex(embeddings, {
+  pineconeIndex,
+  maxConcurrency: 5,
+});
+
 export async function indexDocument(pdfpath) {
   const loader = new PDFLoader(pdfpath, { splitPages: false });
   const doc = await loader.load();
-  console.log(doc[0].pageContent);
+
+  const textSplitter = new RecursiveCharacterTextSplitter({
+    chunkSize: 500,
+    chunkOverlap: 100,
+  });
+
+  const texts = await textSplitter.splitText(doc[0].pageContent);
+
+  const documents = texts.map((chunk) => {
+    return {
+      pageContent: chunk,
+      metadata: doc[0].metadata,
+    };
+  });
+  await vectorStore.addDocuments(documents);
+  // console.log(document);
 }
